27d3ba9f00ee4f14063f5f6c8989d28ac5637d34
lukasagurkas <57620511+lukasagurkas@users.noreply.github.com>
2021-02-12 23:54:40 +0100

    Working User Registration
    Added a DataPicker fragment for date of birth; added radio buttons for gender; user info is stored in firebase database; added a User class

--- app/src/main/java/com/example/healthraceapp/MainActivity.java
+++ app/src/main/java/com/example/healthraceapp/MainActivity.java
@@ -6,26 +6,41 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button buttonRegister;
     private Button buttonSignIn;
+    private EditText editTextUsername;
     private EditText editTextEmail;
     private EditText editTextPassword;
     private ProgressDialog progressDialog;
+    private RadioButton radioButtonMale;
+    private RadioButton radioButtonFemale;
 
     // Defining FirebaseAuth object
     private FirebaseAuth firebaseAuth;
 
+    // Defining FirebaseDatabase object
+    private FirebaseDatabase firebaseDatabase;
+
+    // If the date of birth is set
+    private boolean dateOfBirthSet;
+
+    // Date of birth
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,58 +51,105 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         progressDialog = new ProgressDialog(this);
 
-        buttonRegister =  (Button) findViewById(R.id.buttonRegister);
-        buttonSignIn =  (Button) findViewById(R.id.buttonSignIn);
-
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
+
         buttonRegister.setOnClickListener(this);
         buttonSignIn.setOnClickListener(this);
+
+        radioButtonMale = (RadioButton) findViewById(R.id.radioButtonMale);
+        radioButtonFemale = (RadioButton) findViewById(R.id.radioButtonFemale);
     }
 
-    private void registerUser(){
+    private void registerUser() {
+        String username = editTextUsername.getText().toString().trim();
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
-        //checking if email and passwords are empty
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+        // Checking if username, email and passwords are empty
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "Please enter your username", Toast.LENGTH_LONG).show();
             return;
         }
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter your email address", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_LONG).show();
             return;
         }
 
-        //if the email and password are not empty
-        //displaying a progress dialog
+        if (!dateOfBirthSet) {
+            Toast.makeText(this, "Please set the date of birth", Toast.LENGTH_LONG).show();
+            return;
+        }
 
+        boolean checkedMale = ((RadioButton) radioButtonMale).isChecked();
+        boolean checkedFemale = ((RadioButton) radioButtonFemale).isChecked();
+
+        if (!checkedMale && !checkedFemale) {
+            Toast.makeText(this, "Please select the gender", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // If the email and password are not empty
+        // displaying a progress dialog
         progressDialog.setMessage("Registering Please Wait...");
         progressDialog.show();
 
-        //creating a new user
+        // Creating a new user
         firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        //checking if success
-                        if(task.isSuccessful()){
-                            //display some message here
-                            Toast.makeText(MainActivity.this,"Successfully registered",
+                        if (task.isSuccessful()) {
+                            User user = new User(username, email, checkedMale, year, month, day);
+
+                            firebaseDatabase = FirebaseDatabase.getInstance("https://health-race-app-default-rtdb.europe-west1.firebasedatabase.app/");
+
+                            firebaseDatabase.getReference("Users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "Successfully registered",
+                                                Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, task.getException().getMessage(),
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(MainActivity.this, task.getException().getMessage(),
                                     Toast.LENGTH_LONG).show();
-                        }else{
-                            //display some message here
-                            Toast.makeText(MainActivity.this,"Registration Error," +
-                                    " please try again",Toast.LENGTH_LONG).show();
                         }
                         progressDialog.dismiss();
                     }
                 });
     }
 
+    public void setDateOfBirth(int year, int month, int day) {
+        dateOfBirthSet = true;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
     @Override
     public void onClick(View view) {
-        //calling register method on click
         registerUser();
+    }
+
+    public void showDatePickerDialog(View v) {
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.setCurrentActivity(this);
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
---
+++ app/src/main/java/com/example/healthraceapp/User.java
@@ -0,0 +1,18 @@
+package com.example.healthraceapp;
+
+public class User {
+    public String username, email;
+    public boolean male;
+    public int year, month, day;
+
+    public User() {}
+
+    public User(String username, String email, boolean male, int year, int month, int day) {
+        this.username = username;
+        this.email = email;
+        this.male = male;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
