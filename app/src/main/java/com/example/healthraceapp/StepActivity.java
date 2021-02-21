d06bd34b75e16f9f11cb4b2fb5b980964d8fb7a4
ElianKlaassen <e.t.c.klaassen@student.tue.nl>
2021-02-21 12:35:16 +0100

    Added permission request for step counter

--- app/src/main/java/com/example/healthraceapp/StepActivity.java
+++ app/src/main/java/com/example/healthraceapp/StepActivity.java
@@ -1,21 +1,36 @@
 package com.example.healthraceapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.PeriodicSync;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class StepActivity extends AppCompatActivity implements SensorEventListener {
+    private int PERMISSION_CODE = 1;
     private TextView textViewStepCounter;
     private SensorManager sensorManager;
     private Sensor myStepCounter;
     private boolean isCounterSensorPresent;
     int stepCount = 0;
+    ProgressBar simpleProgressBar; // initiate the progress bar
+//    simpleProgressBar.setMax(100); // 100 maximum value for the progress bar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,20 @@ public class StepActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
+        prog();
+
+        Button buttonRequest = findViewById(R.id.permissionButton);
+        buttonRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(StepActivity.this,
+                        Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(StepActivity.this, "You have already granted this permission!", Toast.LENGTH_SHORT).show();
+                } else {
+                    requestActivity();
+                }
+            }
+        });
         if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
             myStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
             isCounterSensorPresent = true;
@@ -35,12 +64,55 @@ public class StepActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
+    public void prog() {
+        simpleProgressBar  = (ProgressBar) findViewById(R.id.StepProgress);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void requestActivity() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACTIVITY_RECOGNITION)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed because of this and that")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(StepActivity.this, new String[] {Manifest.permission.ACTIVITY_RECOGNITION}, PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACTIVITY_RECOGNITION}, PERMISSION_CODE);
+        }
+
+        ;
+    }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (sensorEvent.sensor == myStepCounter){
             stepCount = (int) sensorEvent.values[0];
             textViewStepCounter.setText(String.valueOf(stepCount));
         }
+        simpleProgressBar.setProgress(stepCount);
     }
 
     @Override
@@ -60,4 +132,5 @@ public class StepActivity extends AppCompatActivity implements SensorEventListen
     protected void onPause() {
         super.onPause();
     }
+
 }
