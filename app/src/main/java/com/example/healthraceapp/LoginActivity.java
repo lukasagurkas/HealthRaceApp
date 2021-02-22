703a64148b47cdc4dd3cad0abbf4bc52bad1b0ff
ElianKlaassen <e.t.c.klaassen@student.tue.nl>
2021-02-22 15:47:40 +0100

    Added permission for step counter to login button, removed from step page

--- app/src/main/java/com/example/healthraceapp/LoginActivity.java
+++ app/src/main/java/com/example/healthraceapp/LoginActivity.java
@@ -1,10 +1,16 @@
 package com.example.healthraceapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -28,6 +34,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     // Defining FirebaseAuth object
     private FirebaseAuth mAuth;
+
+    // If permission is granted
+    private int PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +66,45 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         //attaching click listener
         buttonSignIn.setOnClickListener(this);
         buttonRegister.setOnClickListener(this);
+
+        // Ask for permission to use activity recognition
+        Button buttonRequest = findViewById(R.id.buttonSignIn);
+        buttonRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(LoginActivity.this,
+                        Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(LoginActivity.this, "You have already granted this permission!", Toast.LENGTH_SHORT).show();
+                } else {
+                    requestActivity();
+                }
+            }
+        });
+    }
+
+    // Requests permission to use step counter
+    private void requestActivity() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACTIVITY_RECOGNITION)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed to track the amount of steps you take")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(LoginActivity.this, new String[] {Manifest.permission.ACTIVITY_RECOGNITION}, PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACTIVITY_RECOGNITION}, PERMISSION_CODE);
+        }
+        ;
     }
 
     //method for user login
