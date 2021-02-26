ee4180534a4badc66ad459307300513040c6e3d8
Victoria <victoria.bogachenkova@gmail.com>
2021-02-26 09:06:57 +0100

    Created ProfileActivity and added some UI to it. Further work needs to be done

---
+++ app/src/main/java/com/example/healthraceapp/ProfileActivity.java
@@ -0,0 +1,53 @@
+package com.example.healthraceapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private TextView textEmail, textGender, textUsernameProfile, textDoB;
+    private ImageView userProfileImage;
+
+    private DatabaseReference profileUserRef;
+    private FirebaseAuth mAuth;
+
+    private String currentUserId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        profileUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+
+        textUsernameProfile = (TextView) findViewById(R.id.textUsernameProfile);
+        textEmail = (TextView) findViewById(R.id.textEmailProfile);
+        textDoB = (TextView) findViewById(R.id.textDoB);
+        textGender = (TextView) findViewById(R.id.textGender);
+
+        profileUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
