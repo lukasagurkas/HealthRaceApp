f66a212f21b16180f27340745797fd6f125b7b20
lukasagurkas <57620511+lukasagurkas@users.noreply.github.com>
2021-03-07 21:37:05 +0100

    RecycleView implented in GroupActivity
    Groups, sorting by points, displaying points still needed.

--- app/src/main/java/com/example/healthraceapp/User.java
+++ app/src/main/java/com/example/healthraceapp/User.java
@@ -24,24 +24,48 @@ public class User {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public boolean isMale() {
         return male;
     }
 
+    public void setMale(boolean male) {
+        this.male = male;
+    }
+
     public int getYear() {
         return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     public int getMonth() {
         return month;
     }
 
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
     public int getDay() {
         return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
     }
 
     public int getDailyNumberOfSteps() {
