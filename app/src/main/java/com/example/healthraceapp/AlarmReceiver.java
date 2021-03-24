184d20a594b8937393c31b1d62ae866eaf28335e
Victoria <victoria.bogachenkova@gmail.com>
2021-03-24 13:29:19 +0100

    Comments to alarm receiver

--- app/src/main/java/com/example/healthraceapp/AlarmReceiver.java
+++ app/src/main/java/com/example/healthraceapp/AlarmReceiver.java
@@ -13,18 +13,20 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Log to logcat that the alarm was triggered
         Log.d("Time", "Alarm is triggered");
-
+        //Initiation of all the pages
         WaterActivity waterActivity = new WaterActivity();
         VegetableActivity vegetableActivity = new VegetableActivity();
         FruitActivity fruitActivity = new FruitActivity();
         StepActivity stepActivity = new StepActivity();
 
+        // Switch days for all the health activities
         waterActivity.switchDays();
         vegetableActivity.switchDays();
         fruitActivity.switchDays();
         stepActivity.switchDays();
-
+        // Reset the main page
         MainActivity.reset();
 
 
